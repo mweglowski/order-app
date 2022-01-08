c5756b7f6b1c7dde0a8945d6c101d11eb5fcd0c0
Marcin Weglowski <marcin.weglowski1@gmail.com>
2022-01-08 22:09:32 +0100

    change title in bargain to SectionTitle component

--- src/components/LandingBox/Bargain/Bargain.js
+++ src/components/LandingBox/Bargain/Bargain.js
@@ -1,12 +1,14 @@
 import Card from "../../UI/Card";
 import BrainPill from "../../../images/brain-pill.jpg";
+import SectionTitle from "../../UI/SectionTitle"
+
 import classes from "./Bargain.module.css";
 
 const Bargain = () => {
   return (
     <div className={classes['bargain-box']}>
       <Card className={classes.bargain}>
-        <h1 className={classes.title}>Great Bargain</h1>
+        <SectionTitle className={classes['section-title']} name="Great Bargain" />
         <img src={BrainPill} className={classes.image} alt="brain-pill" />
         <div className={classes.desc}>Brain Pill</div>
         <div className={classes["old-price"]}>1499.99$</div>
